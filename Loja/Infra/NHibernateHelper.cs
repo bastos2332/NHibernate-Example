64e819cae8db9109ea4f2d744335b033e19c8f47
Bastos <gabriel.bastos2332Gmail.com>
2020-05-31 22:37:52 -0400

    adicionando o projeto inicial

---
+++ Loja/Infra/NHibernateHelper.cs
@@ -0,0 +1,28 @@
+﻿using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Infra
+{
+    class NHibernateHelper
+    {
+        public static Configuration RecuperarConfiguracao ()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            return cfg;
+        }
+
+        public static void GeraSchemma()
+        {
+            Configuration cfg = RecuperarConfiguracao();
+            new SchemaExport(cfg).Create(true, true);
+        }
+    }
+}
